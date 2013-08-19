be0055fa99a7357755d4de5f341861cb3e02a52e
Jeremy Senn <jeremysenn@Jeremy-Senns-MacBook-Pro.local>
2013-08-19 06:48:58 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeavyMetalCard::Application.config.secret_token = '1186a627caeaef28db6a96a7ca0c4c97683b75ede40433580dd350531cb2334edc1d5c5458363498b41fa637dd19a719ba3aabd0119787c28febfdaaf92e4166'
